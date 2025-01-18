52829b76568c6bfe1a404112d640f618858b4863
vknanavati <107810028+vknanavati@users.noreply.github.com>
2025-01-17 22:10:21 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello, World, I am Vimin!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
